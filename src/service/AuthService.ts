570424a548ac2bfe32010ee06faef8ffb99e2b82
Tomas <tomassantucho98@gmail.com>
2024-05-10 21:04:09 -0300

    Update AuthService.ts

--- src/service/AuthService.ts
+++ src/service/AuthService.ts
@@ -13,7 +13,7 @@ export interface AuthRequest {
 
 export async function login(authRequest: AuthRequest): Promise<boolean> {
     try {
-        const response = await fetch(`http://localhost:8080/api/auth/login`, {
+        const response = await fetch(`https://table-master.fly.dev/api/auth/login`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -40,7 +40,7 @@ export async function login(authRequest: AuthRequest): Promise<boolean> {
 
 export async function register(registerRequest: RegisterRequest): Promise<boolean> {
     try {
-        const response = await fetch(`http://localhost:8080/api/auth/register`, {
+        const response = await fetch(`https://table-master.fly.dev/api/auth/register`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
